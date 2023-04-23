7ea91217cd78fe7f39d5100643297389d096f131
Deepak Tripathi <40784841+Deepak-Tripathi@users.noreply.github.com>
2023-04-23 15:28:51 +0530

    Update streamlit_app.py
    Typo solve

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,6 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 
-streamlit.stop()
-
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered', fruit_choice)
@@ -40,6 +38,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # Connecting snowflake and streamlit
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
